4fbd6abacb7dba677cb9ddbf5be412c8d9d3bdb3
ortega-daniel <ic.danielortega@gmail.com>
2022-04-06 15:44:32 -0700

    Async apps

---
+++ QuizApp/Program.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace QuizApp
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            var quiz = InitQuiz();
+
+            char startInput;
+            do
+            {
+                Console.Clear();
+                Console.Write("You have 15 seconds to answer 3 questions. Ready to start? (y/n): ");
+                startInput = char.ToLower(Console.ReadLine()[0]);
+            } while (startInput != 'y');
+
+            StartQuizTimer();
+
+            Console.WriteLine("\nPlease indicate your answers as a, b or c. Let's begin!\n");            
+
+            for (int i = 0; i < quiz.Length; i++)
+            {
+                ShowQuestion(quiz, i);
+                char answer = char.ToLower(Console.ReadLine()[0]);
+                quiz[i].Answer = answer;
+                Console.WriteLine();
+            }
+        }
+
+        static QuizItem[] InitQuiz() 
+        {
+            QuizItem q1 = new QuizItem();
+            QuizItem q2 = new QuizItem();
+            QuizItem q3 = new QuizItem();
+            q1.Question = "Cuantos metros son un kilometro?";
+            q1.Options.Add('a', "1000");
+            q1.Options.Add('b', "80");
+            q1.Options.Add('c', "100");
+            q1.CorrectAnswer = 'a';
+
+            q2.Question = "Cual es el elemento 'Ag'?";
+            q2.Options.Add('a', "Argon");
+            q2.Options.Add('b', "Aluminio");
+            q2.Options.Add('c', "Plata");
+            q2.CorrectAnswer = 'c';
+
+            q3.Question = "Cual es la capital de Uruguay?";
+            q3.Options.Add('a', "Maldonado");
+            q3.Options.Add('b', "Artigas");
+            q3.Options.Add('c', "Montevideo");
+            q3.CorrectAnswer = 'c';
+
+            return new QuizItem[] { q1, q2, q3};
+        }
+
+        static void ShowQuestion(QuizItem[] quiz, int question) 
+        {
+            Console.WriteLine(quiz[question].Question);
+            foreach (var item in quiz[question].Options) 
+            {
+                Console.WriteLine($"{item.Key}) {item.Value}");
+            }
+            Console.Write("Your answer: ");
+        }
+
+        static async Task StartQuizTimer() 
+        {
+            var stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            await Task.Delay(5000);
+
+            stopwatch.Stop();
+            Console.WriteLine("Your time is over!");
+        }
+    }
+
+    class QuizItem 
+    {
+        public string Question { get; set; }
+        public Dictionary<char, string> Options{ get; set; } = new Dictionary<char, string>();
+        public char CorrectAnswer { get; set; }
+        public char Answer { get; set; }
+    }
+}
---
+++ Reminder/Program.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Globalization;
+using System.Threading.Tasks;
+using System.Diagnostics;
+
+namespace Reminder
+{
+    internal class Program
+    {
+        static async Task Main(string[] args)
+        {
+            Event myEvent = new Event();
+            DateTime currentDate = DateTime.Now;
+            bool isValidInput = false;
+            int daysLeft;
+
+            // get event name
+            Console.Write("What's your event name?: ");
+            do
+            {
+                myEvent.Name = Console.ReadLine();
+                if (string.IsNullOrEmpty(myEvent.Name) || string.IsNullOrWhiteSpace(myEvent.Name))
+                {
+                    Console.Write("Please enter a valid name: ");
+                }
+                else 
+                {
+                    isValidInput = true;
+                }
+            } while (!isValidInput);
+
+            isValidInput = false;
+
+            // get event date
+            Console.Write("When will it start? Valid date format (mm/dd/yyyy): ");
+            do
+            {
+                if (DateTime.TryParseExact(Console.ReadLine(), "d", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime eventDate))
+                {
+                    myEvent.Date = eventDate;
+                    isValidInput = true;
+                }
+                else 
+                {
+                    Console.Write("Please enter date using a valid format (mm/dd/yyyy): ");
+                }
+            } while (!isValidInput);
+
+            isValidInput = false;
+
+            // get reminder days
+            Console.Write("Please indicate how many days before you want me to start reminding you: ");
+            do
+            {
+                if (Int32.TryParse(Console.ReadLine(), out int reminderDays))
+                {
+                    myEvent.ReminderDays = reminderDays;
+                    isValidInput = true;
+                }
+                else
+                {
+                    Console.Write("Please enter a whole number: ");
+                }
+            } while (!isValidInput);
+            
+            isValidInput = false;
+
+            // get current date
+            Console.Write("Please indicate the current date (mm/dd/yyyy): ");
+            do
+            {
+                if (DateTime.TryParseExact(Console.ReadLine(), "d", CultureInfo.InvariantCulture, DateTimeStyles.None, out DateTime result))
+                {
+                    if (result > myEvent.Date)
+                    {
+                        Console.Write("Event is over! Please enter a different current date:");
+                    }
+                    else 
+                    {
+                        currentDate = result;
+                        isValidInput = true;
+                    }
+                }
+                else
+                {
+                    Console.Write("Please enter date using a valid format (mm/dd/yyyy): ");
+                }
+            } while (!isValidInput);
+
+            daysLeft = (int)(myEvent.Date - currentDate).TotalDays;
+            DateTime reminderDate = myEvent.Date.AddDays(-myEvent.ReminderDays);
+
+            // simulation
+            Console.Clear();
+            for (int i = 0; i < daysLeft; i++)
+            {
+                if (currentDate >= reminderDate)
+                    Console.WriteLine($"{daysLeft - i} days until {myEvent.Name}!");
+                else
+                    Console.WriteLine(currentDate.ToString("d"));
+
+                currentDate = await GetNextDay(currentDate);
+                Console.Clear();
+            }
+            Console.WriteLine($"{myEvent.Name} is today!");
+        }
+
+        public static async Task<DateTime> GetNextDay(DateTime currentDate)
+        {
+            await Task.Delay(1000);
+            return currentDate.AddDays(1);
+        }
+    }
+
+    public class Event 
+    {
+        public string Name { get; set; }
+        public DateTime Date { get; set; }
+        public int ReminderDays { get; set; }
+    }
+}

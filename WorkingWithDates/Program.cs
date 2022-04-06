bc8689cd511f2dab0da555e06e47ce49a24ec710
ortega-daniel <ic.danielortega@gmail.com>
2022-04-06 15:44:15 -0700

    Dates

---
+++ WorkingWithDates/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace WorkingWithDates
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime date = DateTime.Now;
+
+            var x = date.ToString("ddd");
+            Console.WriteLine(x);
+        }
+    }
+}

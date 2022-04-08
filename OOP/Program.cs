171e9a64c4431180ce39c97bac43404ff81f4b98
ortega-daniel <ic.danielortega@gmail.com>
2022-04-07 20:44:51 -0700

    OOP

---
+++ OOP/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+
+namespace OOP
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Square square = new Square(10);
+            Triangle triangle = new Triangle(5, 5);
+
+            Console.WriteLine($"Square area: {square.GetArea()}");
+            Console.WriteLine($"Triangle area: {triangle.GetArea()}");
+        }
+    }
+
+    class Polygon 
+    { 
+        public decimal Width { get; set; }
+        public decimal Height { get; set; }
+
+        public virtual decimal GetArea() 
+        { 
+            return Width * Height;
+        }
+    }
+
+    class Square : Polygon
+    {
+        public Square(decimal side)
+        {
+            Width = side;
+            Height = side;
+        }
+    }
+
+    class Rectangle : Polygon
+    {
+        public Rectangle(decimal width, decimal height)
+        {
+            Width = width;
+            Height = height;
+        }
+    }
+
+    class Triangle : Polygon 
+    {
+        public Triangle(decimal width, decimal height) 
+        { 
+            Width = width;
+            Height = height;
+        }
+
+        public override decimal GetArea() 
+        {
+            return Width * Height / 2;
+        }
+    }
+}

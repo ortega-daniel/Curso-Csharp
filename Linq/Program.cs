58ba7a3585ec7ece1a8ae0a21cbac87d3f4b43b9
ortega-daniel <ic.danielortega@gmail.com>
2022-04-11 16:58:03 -0700

    Linq basics

---
+++ Linq/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Linq
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Func recibe y regresa valores
+            Func<int, int> square = x => x * x;
+            
+            // Action solo recibe valores
+            Action<string> action = x => Console.WriteLine(x);
+
+            Console.WriteLine(square(5));
+            action("Hello world");
+
+            int[] numbers = { 1, 3, 6, 8 };
+            var squaredNumbers = numbers.Select(square);
+            Console.WriteLine(string.Join(",", squaredNumbers));
+
+            // Los actions pueden tambien declararse sin parametros de entrada
+            Action line = () => Console.WriteLine("Hello");
+
+            Func<int, int, string> IsTooLong = (x, y) => x > y ? "es mayor" : "es menor";
+
+            // A partir de c# 7
+            var tuplas = MisTuplas();
+            Console.WriteLine($"{tuplas.Status} Mensaje: {tuplas.Message}");
+
+            Func<int, int, (bool Stats, string Message)> myFunc = (x, y) => (x > y, "Test message");
+
+            var aux = myFunc(1, 2);
+            Console.WriteLine(aux.Message);
+
+            // Se pueden usar delegados con parametros descartados (c# 9)
+            Func<int, int, int> constant = (_, _) => 42;
+            Action<int, int> constant2 = (_, _) => Console.WriteLine(42);
+
+            // Action con serie de declaraciones
+            Action<int> myActionAsync = param =>
+            {
+                Task.Delay(2000);
+                Console.WriteLine("Me espere 2000 ms");
+            };
+
+            Console.WriteLine(IsTooLong(20, 15));
+            Console.WriteLine(IsTooLong2(20, 15));
+
+            // Action con metodo asincrono
+            Action<int> myOtherActionAsync = async param => await DelayConImpresion(param);
+        }
+
+        static async Task DelayConImpresion(int x) 
+        {
+            await Task.Delay(2000);
+            Console.WriteLine($"Me espere 2000 ms e imprimi {x}");
+        }
+
+        static (bool Status, string Message) MisTuplas() 
+        {
+            return (true, "mensaje de prueba");
+        }
+
+        // Metodo con cuerpo de expresion
+        static string IsTooLong2(int x, int y) 
+            => x > y ? "es mayor" : "es menor";
+    }
+}
---
+++ Linq2/Program.cs
@@ -0,0 +1,183 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Linq2
+{
+    internal class Program
+    {
+        private static List<Student> students = new List<Student>
+        {
+            new Student {FirstName="Svetlana", LastName="Omelchenko", ID=111, Scores= new List<int> {97, 92, 81, 60}},
+            new Student {FirstName="Claire", LastName="O'Donnell", ID=112, Scores= new List<int> {75, 84, 91, 39}},
+            new Student {FirstName="Sven", LastName="Mortensen", ID=113, Scores= new List<int> {88, 94, 65, 91}},
+            new Student {FirstName="Cesar", LastName="Garcia", ID=114, Scores= new List<int> {97, 89, 85, 82}},
+            new Student {FirstName="Debra", LastName="Garcia", ID=115, Scores= new List<int> {35, 72, 91, 70}},
+            new Student {FirstName="Fadi", LastName="Fakhouri", ID=116, Scores= new List<int> {99, 86, 90, 94}},
+            new Student {FirstName="Hanying", LastName="Feng", ID=117, Scores= new List<int> {93, 92, 80, 87}},
+            new Student {FirstName="Hugo", LastName="Garcia", ID=118, Scores= new List<int> {92, 90, 83, 78}},
+            new Student {FirstName="Lance", LastName="Tucker", ID=119, Scores= new List<int> {68, 79, 88, 92}},
+            new Student {FirstName="Terry", LastName="Adams", ID=120, Scores= new List<int> {99, 82, 81, 79}},
+            new Student {FirstName="Eugene", LastName="Zabokritski", ID=121, Scores= new List<int> {96, 85, 91, 60}},
+            new Student {FirstName="Michael", LastName="Tucker", ID=122, Scores= new List<int> {94, 92, 91, 91}}
+        };
+
+        static void Main(string[] args)
+        {
+            int[] scores = { 94, 76, 95, 67, 45 };
+
+            // Usando LINQ to Objects en formato query expression
+            IEnumerable<int> scoreQuery = from score in scores
+                                          where score > 70
+                                          select score * score;
+
+            // Usando linq con metodos de extension y expresiones lambda
+            var scoreQueryLambda = scores
+                .Where(score => score > 70)
+                .Select(score => score * score);
+
+            /*foreach (var item in scoreQuery)
+            {
+                Console.Write($"{item} ");
+            }*/
+
+            var studentQuery =
+                from student in students
+                where student.Scores[0] > 90 && student.Scores[3] < 80
+                orderby student.Scores[0] descending
+                select student;
+
+            var studentQueryLambda = students
+                .Where(s => s.Scores[0] > 90 && s.Scores[3] < 80)
+                .OrderByDescending(s => s.Scores[0]);
+
+            var studentQueryLambdaX = students
+                // All regresara solo si la condicion se cumple para todos los elementos de la lista
+                .Where(student => student.Scores.All(score => score > 80))
+                // Any regresara solo si la condicion se cumple en 1 de los elementos de la lista
+                //.Where(student => student.Scores.All(score => score > 80))
+                .OrderByDescending(student => student.Scores[0]);
+
+            /*foreach (var student in studentQuery)
+            {
+                Console.WriteLine($"{student.LastName} {student.FirstName} {student.Scores[0]}");
+            }*/
+
+            var studentQuery2 =
+                from student in students
+                group student by student.LastName[0]
+                into studentGroup
+                orderby studentGroup.Key
+                select studentGroup;
+
+            var studentQuery2Lambda = students
+                .GroupBy(student => student.LastName[0])
+                .OrderBy(group => group.Key);
+
+            foreach (var studentGroup in studentQuery2)
+            {
+                Console.WriteLine(studentGroup.Key);
+
+                foreach (var student in studentGroup)
+                {
+                    Console.WriteLine($"nombre: {student.LastName} apellido: {student.FirstName}");
+                }
+            }
+
+            // Se puede utilizar linq para realizar operaciones dentro de la comparacion y ademas regresar el valor deseado ya formateado
+            var studentQuery3 =
+                from student in students
+                let totalScore = student.Scores[0] + student.Scores[1] + student.Scores[2] + student.Scores[3]
+                where totalScore / 4 < student.Scores[0]
+                select $"{student.FirstName} {student.LastName} {totalScore / 4}";
+
+            var studentQuery3Lambda = students
+                .Where(student => (student.Scores[0] + student.Scores[1] + student.Scores[2] + student.Scores[3]) / 4 < student.Scores[0])
+                .Select(student => $"{student.FirstName} {student.LastName}");
+
+            /*foreach (var item in studentQuery3)
+            {
+                Console.WriteLine(item);
+            }*/
+
+            var studentQuery4 =
+                from student in students
+                let totalScore = student.Scores.Sum() / student.Scores.Count
+                where totalScore < student.Scores[0]
+                select $"{student.FirstName} {student.LastName} {totalScore}";
+
+            var studentQuery4Lambda = students
+                .Where(student => student.Scores.Sum() / 4 < student.Scores[0])
+                .Select(student => $"{student.FirstName} {student.LastName}");
+
+            var studentQuery5 =
+                from student in students
+                let totalScore = student.Scores.Average()
+                where totalScore < student.Scores[0]
+                select $"{student.FirstName} {student.LastName} {totalScore}";
+
+            var studentQuery5Lambda = students
+                .Where(student => student.Scores.Average() < student.Scores[0])
+                .Select(student => $"{student.FirstName} {student.LastName}");
+
+            // Sacar el promedio general de toda la clase
+            var promedioGeneral = students.Average(s => s.Scores.Average());
+
+            // Tambien se pueden hacer consultar linq con variable externas a ellas
+            string lastName = "Garcia";
+            var studentQuery6 =
+                from student in students
+                where student.LastName.Equals(lastName)
+                select student.FirstName;
+
+            var studentQuery6Lambda = students
+                .Where(student => student.LastName.Equals(lastName))
+                .Select(student => student.FirstName);
+
+            /*Console.WriteLine($"Todos los {lastName} de la clase son:");
+            Console.WriteLine(String.Join(", ", studentQuery6));*/
+
+            // Podemos crear nuevos objetos a partir de nuestra consulta
+            var studentQuery7 =
+                from student in students
+                let totalScore = student.Scores.Sum()
+                where totalScore > promedioGeneral
+                select new { ID = student.ID, Score = totalScore };
+
+            var studentQuery7Lambda = students
+                .Where(student => student.Scores.Sum() > promedioGeneral)
+                .Select(student => new 
+                { 
+                    ID = student.ID, 
+                    Score = student.Scores.Sum()
+                });
+
+            var primerEstudianteFiltrado = studentQueryLambdaX
+                .Where(s => s.ID > 1000)
+                .FirstOrDefault();
+
+            var primerEstudianteFiltradoReducido = studentQueryLambdaX
+                .FirstOrDefault(s => s.ID > 1000);
+
+            Console.WriteLine(primerEstudianteFiltrado == null ? "es nulo" : primerEstudianteFiltrado.FirstName);
+
+            var ultimoEstudianteFiltrado = studentQueryLambdaX.Last();
+
+            Student single = studentQueryLambdaX
+                .SingleOrDefault(s => s.ID == 101);
+
+            foreach (var item in studentQuery7)
+            {
+                Console.WriteLine($"Student id: {item.ID} Score: {item.Score}");
+            }
+        }
+    }
+
+    class Student 
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public int ID { get; set; }
+        public List<int> Scores { get; set; }
+    }
+}
